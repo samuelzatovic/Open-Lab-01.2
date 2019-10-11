0f14a5dadbabf29532eebbc3f053a702658081cb
samuel zaťovič <szatovic@gmail.com>
2019-10-11 19:23:33 +0200

    Hotový program

---
+++ Open-Lab-01.2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Open_Lab_01._3
+{
+    class Program
+    {
+        public static int Sum(int number1, int number2)
+        {
+            int result = number1 + number2;
+            return result;
+        }
+    }
+}
